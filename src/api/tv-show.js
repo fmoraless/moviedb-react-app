dbd308c46cb552134a4f430f12cc5069897a7f80
fmoraless <franticzero@gmail.com>
2023-03-14 23:49:51 -0300

    feat(search): :sparkles: add search bar

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -7,23 +7,20 @@ export class TVShowAPI {
     static  async fetchPopulars() {
         try {
             const response = await axios.get(`${BASE_URL}/tv/popular${API_KEY_PARAM}`)
-            console.log(response.data.results)
+            //console.log(response.data.results)
             return response.data.results
         } catch (error) {
             console.log(error)
         }
     }
 
-    /* version con FETCH */
-    /* static  async fetchPopulars() {
+    static  async fetchByTitle(title) {
         try {
-            const response = await fetch(`${API_URL}/tv/popular${API_KEY_PARAM}`, {
-                method: 'GET',
-            })
-            const results = await response.json();
-            return results.results
+            const response = await axios.get(`${BASE_URL}/search/tv${API_KEY_PARAM}&query=${title}`)
+            console.log(response.data.results)
+            return response.data.results
         } catch (error) {
             console.log(error)
         }
-    } */
+    }
 }
