d5c759b52f4197aada972e0d6bfc89f595947b01
fmoraless <franticzero@gmail.com>
2023-03-18 16:22:43 -0300

    build(core-app): customize vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,27 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3030,
+  },
+  preview: {
+    port: 3031,
+  },
+  build: {
+    incremental: true,
+    babel: {
+      pressets: ['@babel/preset-env','@babel/preset-react'],
+    },
+    /* when project use Typescript */
+    /* typescript: {
+      tsconfig: './tsconfig.json',
+    }, */
+    cache: true,
+    minify: true,
+    mode: 'production',
+    chunks: true,
+    moduleBuildings: true,
+    devCode: true,
+    debug: true,
+  }
 })
